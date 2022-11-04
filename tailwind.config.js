902bd0d9900aada41705e937a0fe91014d94a981
NevercomeG <luisjavier2012@gmail.com>
2022-11-04 00:15:10 -0400

    1.14b

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,19 @@ module.exports = {
       'gray': '#8492a6',
       'gray-light': '#d3dce6',
     },
-    extend: {},
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    }
   },
   plugins: [],
 }
