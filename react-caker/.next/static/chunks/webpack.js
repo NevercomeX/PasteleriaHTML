b0329d525896d46e1367e33ffc56e9dcb6327818
NevercomeG <luisjavier2012@gmail.com>
2022-11-10 23:53:24 -0400

    added admin and login screen still on alpha

--- react-caker/.next/static/chunks/webpack.js
+++ react-caker/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca4a7880be13be36"; }
+/******/ 		__webpack_require__.h = function() { return "e4bd545a0439de3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
